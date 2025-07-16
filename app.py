72f3236f9269ac9e5922b8b107352ad309bb4527
Wang Zhulin <wangzhulin@example.com>
2025-07-16 15:24:04 +0800

    Add CORS support

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import os
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from crawler.taobao import search_price as taobao_price
 from crawler.jd import search_price as jd_price
 from crawler.dangdang import search_price as dangdang_price
 
 app = Flask(__name__)
+CORS(app)  # 允许所有来源跨域请求
 
 @app.route('/api/price', methods=['POST'])
 def price():
